f8570c92dc878b74d1336d82af59927fbc509185
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-23 21:38:47 -0300

    feat: adicionou funcao scrape_noticia - requisito 4

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -4,9 +4,6 @@ import requests
 import time
 
 
-# html = fetch("https://blog.betrybe.com/")
-
-
 # Requisito 1
 def fetch(url):
     try:
@@ -39,7 +36,39 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    url = selector.css("link[rel='canonical']::attr(href)").get()
+    title = selector.css("h1.entry-title::text").get()
+    timestamp = selector.css("li.meta-date::text").get()
+    writer = selector.css("span.author a::text").get()
+    comments_count = selector.css("div[id='comments'] h5::text").get()
+
+    summary = selector.css(
+        "div.entry-content > p:first-of-type *::text"
+    ).getall()  # lista com todos os elementos do primeiro p
+
+    # convertendo lista em string
+    # https://www.delftstack.com/pt/howto/python/how-to-convert-a-list-to-string/
+
+    summary_content = "".join(summary).strip()
+    tags = selector.css("a[rel='tag']::text").getall()
+    category = selector.css("span.label::text").get()
+
+    if comments_count is not None:
+        comments_count_number = int(comments_count.split("comments")[0])
+    else:
+        comments_count_number = 0
+
+    return {
+        "url": url,
+        "title": title.strip(),
+        "timestamp": timestamp,
+        "writer": writer,
+        "comments_count": comments_count_number,
+        "summary": summary_content,
+        "tags": tags,
+        "category": category,
+    }
 
 
 # Requisito 5
