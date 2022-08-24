76d9d1d37c7a485d51ed1bd2af5ec8940db6d248
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-24 17:44:16 -0300

    feat:  adicionou funcao top_5_news - requisito 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,24 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    search_result = find_news()
+    search_result.sort(key=lambda x: x["title"])
+    search_result.sort(reverse=True, key=lambda x: x["comments_count"])
+    top_5_news_list = []
+
+    if len(search_result) < 5:
+        max_length = len(search_result)
+    else:
+        max_length = 5
+
+    for index in range(0, max_length):
+        top_5_news_list.append(
+            (search_result[index]["title"], search_result[index]["url"])
+        )
+
+    return top_5_news_list
 
 
 # Requisito 11
