7723b84fc616e1e3475f8f7cf57e10484243a49d
Rafel Nakashima <rafaelnakashima@rocketmail.com>
2022-08-24 22:46:24 -0300

    feat: adicionou funcionalidades da funcao analyzer_menu

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,4 +1,12 @@
 import sys
+from tech_news.scraper import get_tech_news
+from tech_news.analyzer.search_engine import (
+    search_by_title,
+    search_by_date,
+    search_by_tag,
+    search_by_category,
+)
+from tech_news.analyzer.ratings import top_5_news, top_5_categories
 
 
 # Requisito 12
@@ -20,13 +28,36 @@ def analyzer_menu():
         "",
         "",
     ]
+    function_options = [
+        get_tech_news,
+        search_by_title,
+        search_by_date,
+        search_by_tag,
+        search_by_category,
+        top_5_news,
+        top_5_categories,
+    ]
     try:
         if 0 <= int(i) <= 7:
-            print(options[int(i)])
+            return conditonal_reponse(i, options, function_options)
         else:
             print("Opção inválida", file=sys.stderr)
     except ValueError:
         print("Opção inválida", file=sys.stderr)
+
+
+def conditonal_reponse(i, options, function_options):
+    if int(i) == 0:
+        response = input(options[int(i)])
+        return function_options[int(i)](int(response))
+    elif int(i) == 7:
+        print("Encerrando script")
+    else:
+        if options[int(i)] != "":
+            response = input(options[int(i)])
+            return function_options[int(i)](str(response))
+        else:
+            return function_options[int(i)]()
 
 
 # utilizar stderr
